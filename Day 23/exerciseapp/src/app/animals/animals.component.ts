95f190904b18f2c63548c79f0676fd0404693d69
conscious27 <chaitanyapradhan27@gmail.com>
2023-08-09 16:33:18 +0530

    Updated

--- Day 23/exerciseapp/src/app/animals/animals.component.ts
+++ Day 23/exerciseapp/src/app/animals/animals.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./animals.component.css']
 })
 export class AnimalsComponent {
-
+  animals:any[]=[
+    {aname:'Tiger', species:'Feline', photos:'https://th.bing.com/th/id/OIP.jF-kCTt5tK99_v5uiStPAAHaFj?w=252&h=189&c=7&r=0&o=5&pid=1.7'},
+    {aname:'Parrot', species:'Bird', photos:'https://th.bing.com/th/id/OIP.qCbJl0cMcZu8wcPfmg7QnQHaE7?w=238&h=180&c=7&r=0&o=5&pid=1.7'},
+    {aname:'Finray', species:'Fish', photos:'https://th.bing.com/th/id/OIP.iNkAKTBZPaGAWTrZKTBGEAHaFQ?w=278&h=197&c=7&r=0&o=5&pid=1.7'}
+  ]
 }
